1450d219b162ff8e8687cda1c05f30aee56232ef
Samkelo Gigaba <sgigaba@student.wethinkcode.co.za>
2023-05-25 10:10:00 +0200

    Add SignalR secret to csproj

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMvc();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -15,6 +17,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseEndpoints(endpoints =>
+{
+   /* endpoints.MapHub<NotifyHub>("/notify");*/
+});
 
 app.UseHttpsRedirection();
 
