01e2718ed80a333aaaa94cbf11c79f3c4f4e73ff
Mpho Jele <mphoj@payspace.com>
2023-05-30 09:28:26 +0200

    Post form with image upload

---
+++ reactapp/src/components/MediaUpload.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from "react";
+import { Button } from "@mui/material";
+import useForm from "../hooks/useForm";
+
+type UploadValues = {
+    avatar: string;
+    avatarFile: any;
+}
+
+interface MediaUploadProps {
+    mediaFileGetter: Function;
+}
+
+function MediaUpload({ mediaFileGetter }: MediaUploadProps) {
+    const fileInput = useRef<HTMLInputElement | null>(null);
+    const { onChange, values } = useForm<UploadValues>(formSubmit, {
+        avatar: "",
+        avatarFile: null,
+    });
+
+    function formSubmit() {
+        console.log("Form submitted", values);
+    }
+
+    useEffect(() => {
+        if (values.avatarFile) {
+            mediaFileGetter(values.avatarFile);
+        }
+    }, [values, mediaFileGetter]);
+
+    return (
+        <div>
+            <form>
+                <input ref={fileInput} style={{ display: "none" }} type="file"
+                    id="avatar" name="avatar"
+                    accept="image/png, image/jpeg" onChange={onChange} />
+                <Button variant="contained" onClick={(_) => {
+                    fileInput.current?.click();
+                }}>
+                    upload
+                </Button>
+            </form>
+
+            <img src={values.avatar} alt="" />
+        </div>
+    );
+}
+
+export default MediaUpload;
