8e9c437382dd3d1ddedfa6d643e62df79ce221e5
Mpho Jele <mphoj@payspace.com>
2023-05-30 09:57:52 +0200

    Add Post table to migrations

--- webapi/Migrations/20230530075729_AddPostTable.Designer.cs
+++ webapi/Migrations/20230530075729_AddPostTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webapi.Data;
 
@@ -11,9 +12,11 @@ using Webapi.Data;
 namespace Webapi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230530075729_AddPostTable")]
+    partial class AddPostTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
